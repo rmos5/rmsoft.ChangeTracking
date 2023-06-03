e30150ab04318e37e98610f20a56ce095fa0ff21
rmos5 <raivo.mosona@gmail.com>
2023-06-03 22:14:53 +0300

    TestApp updates.

--- rmsoft.ChangeTracking.TestApp/MainContext.Commands.cs
+++ rmsoft.ChangeTracking.TestApp/MainContext.Commands.cs
@@ -31,206 +31,46 @@ namespace rmsoft.ChangeTracking.TestApp
         private readonly Random _random = new Random();
         private void ExecuteGetDataCommand(object? parameter)
         {
-            _data.Clear();
+            this.Clear();
             int num = _random.Next(5, 20);
             int num1 = 1;
             NamedObject obj;
             for (int i = 0; i < num; i++)
             {
-                obj = new NamedObject($"Name{num1}", $"Description{num1}");
-                obj.TrackerUpdated += (s, e) => RefreshModelState();
-                _data.Add(obj);
+                obj = CreateAndAddNewData(i);
                 num1++;
             }
-
-            SelectedItem = _data.Last();
         }
 
-
-        public class AddDataCommandImpl : ContextCommandBase<MainContext>
+        public class AddNewDataCommandImpl : ContextCommandBase<MainContext>
         {
-            public AddDataCommandImpl(MainContext context) : base(context)
+            public AddNewDataCommandImpl(MainContext context) : base(context)
             {
             }
 
             public override bool CanExecute(object parameter)
             {
-                return Context.CanExecuteAddDataCommand(parameter);
+                return Context.CanExecuteAddNewDataCommand(parameter);
             }
 
             public override void Execute(object parameter)
             {
-                Context.ExecuteAddDataCommand(parameter);
+                Context.ExecuteAddNewDataCommand(parameter);
             }
         }
 
-        private bool CanExecuteAddDataCommand(object parameter)
+        private bool CanExecuteAddNewDataCommand(object parameter)
         {
-            return _dataChangeTracker.IsTracking;
+            return this.IsTracking;
         }
 
-        private void ExecuteAddDataCommand(object parameter)
+        static int Number = 1000;
+        private void ExecuteAddNewDataCommand(object parameter)
         {
-            int num = _data.Count + 1;
-            NamedObject obj = new NamedObject($"Name{num}", $"Description{num}");
-            if (IsAppendData)
-                _data.Add(obj);
-            else
-                _data.Insert(0, obj);
-            SelectedItem = obj;
-        }
+            if (Count == 0)
+                Number = 1000;
 
-        public class RemoveDataCommandImpl : ContextCommandBase<MainContext>
-        {
-            public RemoveDataCommandImpl(MainContext context) : base(context)
-            {
-            }
-
-            public override bool CanExecute(object parameter)
-            {
-                return Context.CanExecuteRemoveDataCommand(parameter);
-            }
-
-            public override void Execute(object parameter)
-            {
-                Context.ExecuteRemoveDataCommand(parameter);
-            }
-        }
-
-        private bool CanExecuteRemoveDataCommand(object parameter)
-        {
-            return _dataChangeTracker.IsTracking
-                && HasSelectedItem
-                && !SelectedItem.HasChanges;
-        }
-
-        private void ExecuteRemoveDataCommand(object parameter)
-        {
-            int index = _data.IndexOf(SelectedItem);
-            _data.RemoveAt(index);
-            index--;
-
-            SelectedItem = index >= 0 ? _data[index] : null;
-        }
-
-        public class ToggleDataEditCommandImpl : ContextCommandBase<MainContext>
-        {
-            public ToggleDataEditCommandImpl(MainContext context)
-                : base(context)
-            {
-            }
-
-            public override bool CanExecute(object parameter)
-            {
-                return Context.CanExecuteToggleDataEditCommand(parameter);
-            }
-
-            public override void Execute(object parameter)
-            {
-                Context.ExecuteToggleDataEditCommand(parameter);
-            }
-        }
-
-        private bool CanExecuteToggleDataEditCommand(object parameter)
-        {
-            return true;
-        }
-
-        private void ExecuteToggleDataEditCommand(object parameter)
-        {
-            if (_dataChangeTracker.IsTracking)
-                _dataChangeTracker.StopTracking(true);
-            else
-                _dataChangeTracker.StartTracking();
-
-            RefreshModelState();
-        }
-
-        public class UndoDataChangesCommandImpl : ContextCommandBase<MainContext>
-        {
-            public UndoDataChangesCommandImpl(MainContext context)
-                : base(context)
-            {
-            }
-
-            public override bool CanExecute(object parameter)
-            {
-                return Context.CanExecuteUndoDataChangesCommand(parameter);
-            }
-
-            public override void Execute(object parameter)
-            {
-                Context.ExecuteUndoDataChangesCommand(parameter);
-            }
-        }
-
-        private bool CanExecuteUndoDataChangesCommand(object parameter)
-        {
-            return _dataChangeTracker.CanUndo();
-        }
-
-        private void ExecuteUndoDataChangesCommand(object parameter)
-        {
-            _dataChangeTracker.Undo();
-            RefreshModelState();
-        }
-
-        public class RedoDataChangesCommandImpl : ContextCommandBase<MainContext>
-        {
-            public RedoDataChangesCommandImpl(MainContext context)
-                : base(context)
-            {
-            }
-
-            public override bool CanExecute(object parameter)
-            {
-                return Context.CanExecuteRedoDataChangesCommand(parameter);
-            }
-
-            public override void Execute(object parameter)
-            {
-                Context.ExecuteRedoDataChangesCommand(parameter);
-            }
-        }
-
-        private bool CanExecuteRedoDataChangesCommand(object parameter)
-        {
-            return _dataChangeTracker.CanRedo();
-        }
-
-        private void ExecuteRedoDataChangesCommand(object parameter)
-        {
-            _dataChangeTracker.Redo();
-            RefreshModelState();
-        }
-
-        public class ApplyDataChangesCommandImpl : ContextCommandBase<MainContext>
-        {
-            public ApplyDataChangesCommandImpl(MainContext context)
-                : base(context)
-            {
-            }
-
-            public override bool CanExecute(object parameter)
-            {
-                return Context.CanExecuteApplyDataChangesCommand(parameter);
-            }
-
-            public override void Execute(object parameter)
-            {
-                Context.ExecuteApplyDataChangesCommand(parameter);
-            }
-        }
-
-        private bool CanExecuteApplyDataChangesCommand(object parameter)
-        {
-            return _dataChangeTracker.CanUndo();
-        }
-
-        private void ExecuteApplyDataChangesCommand(object parameter)
-        {
-            _dataChangeTracker.StopTracking(false);
-            RefreshModelState();
+            CreateAndAddNewData(Number++);
         }
     }
 }
--- rmsoft.ChangeTracking.TestApp/MainContext.cs
+++ rmsoft.ChangeTracking.TestApp/MainContext.cs
@@ -1,106 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Linq;
+﻿using System.Linq;
 
 namespace rmsoft.ChangeTracking.TestApp
 {
-    public partial class MainContext : TrackedObjectBase, IEnumerable<NamedObject>, INotifyCollectionChanged
+    public partial class MainContext : TrackedObservableCollection<NamedObject>
     {
-        public class ObservableList<T> : ObservableCollection<T>, INotifyListChanged<T>
+        private bool isInsertData;
+
+        public bool IsInsertData
         {
-        }
-
-        public event NotifyCollectionChangedEventHandler? CollectionChanged;
-
-        private readonly INotifyListChanged<NamedObject> _data;
-
-        private readonly ListChangeTracker<NamedObject> _dataChangeTracker;
-
-        private NamedObject? selectedItem;
-
-        public NamedObject? SelectedItem
-        {
-            get => selectedItem;
+            get => isInsertData;
             set
             {
-                if (selectedItem != value)
+                if (value != isInsertData)
                 {
-                    selectedItem = value;
-                    RefreshModelState();
+                    isInsertData = value;
+                    OnPropertyChanged(nameof(IsInsertData));
                 }
             }
         }
-
-        private bool isAppendData;
-
-        public bool IsAppendData
-        {
-            get => isAppendData;
-            set 
-            { 
-                if (value != isAppendData)
-                {
-                    isAppendData = value;
-                    OnPropertyChanged(nameof(IsAppendData));
-                }
-            }
-        }
-
-
-        public override bool IsTracking =>
-            base.IsTracking
-            || _data.Any(o => o.IsTracking);
-
-        public bool HasSelectedItem => SelectedItem != null;
 
         public IContextCommand GetDataCommand { get; }
 
-        public IContextCommand ToggleDataEditCommand { get; }
-
-        public IContextCommand AddDataCommand { get; }
-
-        public IContextCommand RemoveDataCommand { get; }
-
-        public IContextCommand UndoDataChangesCommand { get; }
-
-        public IContextCommand RedoDataChangesCommand { get; }
-
-        public IContextCommand ApplyDataChangesCommand { get; }
+        public IContextCommand AddNewDataCommand { get; }
 
         public MainContext()
         {
             GetDataCommand = new GetDataCommandImpl(this);
-            ToggleDataEditCommand = new ToggleDataEditCommandImpl(this);
-            AddDataCommand = new AddDataCommandImpl(this);
-            RemoveDataCommand = new RemoveDataCommandImpl(this);
-            UndoDataChangesCommand = new UndoDataChangesCommandImpl(this);
-            RedoDataChangesCommand = new RedoDataChangesCommandImpl(this);
-            ApplyDataChangesCommand = new ApplyDataChangesCommandImpl(this);
-
-            _data = new ObservableList<NamedObject>();
-            _dataChangeTracker = new ListChangeTracker<NamedObject>(_data);
-            _dataChangeTracker.TrackerUpdated += _dataChangeTracker_TrackerUpdated;
-            _data.CollectionChanged += _data_CollectionChanged;
-        }
-
-        ~MainContext()
-        {
-            _dataChangeTracker.TrackerUpdated -= _dataChangeTracker_TrackerUpdated;
-            _data.CollectionChanged -= _data_CollectionChanged;
-        }
-
-        private void _dataChangeTracker_TrackerUpdated(object? sender, System.EventArgs e)
-        {
-        }
-
-        private void _data_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
-        {
-            RefreshModelState();
-            NotifyCollectionChangedEventHandler? h = CollectionChanged;
-            h?.Invoke(this, e);
+            AddNewDataCommand = new AddNewDataCommandImpl(this);
+            IsTrackingEnabled = true;
         }
 
         public override void RefreshModelState()
@@ -110,22 +37,20 @@ namespace rmsoft.ChangeTracking.TestApp
             OnPropertyChanged(nameof(HasSelectedItem));
 
             GetDataCommand.RaiseCanExecuteChanged();
-            ToggleEditingCommand.RaiseCanExecuteChanged();
-            AddDataCommand.RaiseCanExecuteChanged();
-            RemoveDataCommand.RaiseCanExecuteChanged();
-            UndoDataChangesCommand.RaiseCanExecuteChanged();
-            RedoDataChangesCommand.RaiseCanExecuteChanged();
-            ApplyDataChangesCommand.RaiseCanExecuteChanged();
+            AddNewDataCommand.RaiseCanExecuteChanged();
         }
 
-        public IEnumerator<NamedObject> GetEnumerator()
+        private NamedObject CreateAndAddNewData(int number)
         {
-            return _data.GetEnumerator();
-        }
+            NamedObject result = new NamedObject($"Name{number}", $"Description{number}");
+            result.IsTrackingEnabled = true;
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return this.GetEnumerator();
+            if (IsInsertData)
+                Insert(0, result);
+            else
+                Add(result);
+
+            return result;
         }
     }
 }
