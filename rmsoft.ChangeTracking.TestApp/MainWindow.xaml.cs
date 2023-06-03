67267a5c2d3aed8d6375b3b2d86c0cfa9200a0cf
rmos5 <raivo.mosona@gmail.com>
2023-06-03 22:35:17 +0300

    MainWindow updates.

--- rmsoft.ChangeTracking.TestApp/MainWindow.xaml.cs
+++ rmsoft.ChangeTracking.TestApp/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace rmsoft.ChangeTracking.TestApp
 {
@@ -20,6 +7,12 @@ namespace rmsoft.ChangeTracking.TestApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public new MainContext DataContext
+        {
+            get => (MainContext)base.DataContext;
+            set => base.DataContext = value;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
