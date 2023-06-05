e7ac4e82de108bc79d188a7229cf2a667c3f3d5d
Raivo Mosona <raivo.mosona@gmail.com>
2023-06-05 18:52:18 +0300

    SharedAssemblyInfo.cs link to physical copy.

---
+++ rmsoft.ChangeTracking/Properties/SharedAssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+using System.Windows.Markup;
+
+[assembly: AssemblyCompany("rmsoft")]
+[assembly: AssemblyCopyright("©2019-2023 rmsoft")]
+[assembly: AssemblyTrademark("rmsoft™")]
