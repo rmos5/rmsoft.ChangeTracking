ad3f6e83e042d3a95bb0c067578427d6d4fd7673
Raivo Mosona <raivo.mosona@gmail.com>
2023-06-05 18:26:12 +0300

    SharedAssemblyInfo.cs location update; folder remove.

--- SharedAssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Windows.Markup;
-
-//Commmon Rmsoft attributes
-[assembly: AssemblyCompany("Rmsoft")]
-[assembly: AssemblyCopyright("©2019-2021 Rmsoft Oy")]
-[assembly: AssemblyTrademark("Rmsoft™")]
-
-[assembly: XmlnsPrefix("http://schemas.rmsoft.fi/controls", "rmcontrols")]
