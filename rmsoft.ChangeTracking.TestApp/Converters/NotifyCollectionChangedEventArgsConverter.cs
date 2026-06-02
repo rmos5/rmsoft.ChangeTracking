7997b414203b9b53ac60ea244adaa554c6cbd8f5
rmos5 <raivo.mosona@gmail.com>
2026-06-02 15:52:19 +0300

    Clean up WPF test app

--- rmsoft.ChangeTracking.TestApp/Converters/NotifyCollectionChangedEventArgsConverter.cs
+++ rmsoft.ChangeTracking.TestApp/Converters/NotifyCollectionChangedEventArgsConverter.cs
@@ -1,36 +1,34 @@
-﻿using System;
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Globalization;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Data;
 
 namespace rmsoft.ChangeTracking.TestApp.Converters
 {
-    public class NotifyPropertyChangedEventArgsConverter : IValueConverter
+    public class NotifyCollectionChangedEventArgsConverter : IValueConverter
     {
-        private object GetItemsString(IList? items)
+        private static object GetItemsString(IList? items)
         {
             if (items == null
                 || items.Count == 0)
                 return "(None)";
 
             StringBuilder sb = new StringBuilder();
-            foreach(object obj in items)
+            foreach (object obj in items)
             {
                 sb.Append(obj.ToString());
                 sb.Append(' ');
             }
 
-            return $"{{{sb.ToString()}}}";
+            return $"{{{sb}}}";
         }
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            NotifyCollectionChangedEventArgs val = (NotifyCollectionChangedEventArgs)value;
+            if (value is not NotifyCollectionChangedEventArgs val)
+                return string.Empty;
 
             return $"{val.Action} {val.NewStartingIndex} {GetItemsString(val.NewItems)} {val.OldStartingIndex} {GetItemsString(val.OldItems)}";
         }
